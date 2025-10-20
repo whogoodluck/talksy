b7be4ecb9d6415523cca63fe895300543901c139
sunny <goodluck14115@gmail.com>
2025-10-20 15:08:26 +0530

    Setup React + Vite with Tailwind

---
+++ web/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-5xl font-bold">Hello World!</h1>
+    </div>
+  )
+}
+
+export default App
