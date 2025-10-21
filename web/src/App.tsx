3d50b32a92fbc44208cce38113a950494eeb7763
sunny <goodluck14115@gmail.com>
2025-10-21 20:20:33 +0530

    Setup prettier & eslint in web for better code formating and linting

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <h1 className="text-5xl font-bold">Hello World!</h1>
+    <div className='flex h-screen items-center justify-center'>
+      <h1 className='text-5xl font-bold'>Hello World!</h1>
     </div>
   )
 }
