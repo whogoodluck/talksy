f382ea7c5b157fbf6383779bf9ae467c2a2f0674
sunny <goodluck14115@gmail.com>
2025-12-13 01:39:41 +0530

    Refactor

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,10 @@
 import { PrismaClient } from '@prisma/client'
+import config from '../utils/config'
 
-export const prisma = new PrismaClient()
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+
+if (config.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
