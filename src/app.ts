51e4d6344a80294570483719b347f98856f25501
sunny <goodluck14115@gmail.com>
2025-12-12 23:52:34 +0530

    Unified backend + frontend deployment

--- src/app.ts
+++ src/app.ts
@@ -5,21 +5,49 @@ import morgan from 'morgan'
 
 import userRouter from './routes/user.route'
 
+import path from 'path'
 import errorHandler from './middlewares/error-handler'
 import unknownEndpoint from './middlewares/unknown-endpoint'
 
 const app = express()
 
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(cors())
 app.use(cookieParser())
 
-app.get('/', (_req: Request, res: Response) => {
-  res.send('Hello World!')
-})
-
 app.use('/api/v1/users', userRouter)
+
+// React App
+app.use(
+  express.static(path.join(__dirname, '../web/dist'), {
+    setHeaders(res, filePath) {
+      res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin')
+      res.setHeader('X-Content-Type-Options', 'nosniff')
+
+      if (filePath.endsWith('index.html')) {
+        res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
+        res.setHeader('Pragma', 'no-cache')
+        res.setHeader('Expires', '0')
+      } else {
+        res.setHeader('Cache-Control', 'public, max-age=31536000, immutable')
+      }
+    },
+  })
+)
+
+app.use((req: Request, res: Response, next) => {
+  if (req.path.startsWith('/api')) {
+    return next()
+  }
+
+  res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin')
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
+  res.setHeader('X-Content-Type-Options', 'nosniff')
+
+  res.sendFile(path.join(__dirname, '../web/dist/index.html'))
+})
 
 app.use(errorHandler)
 app.use(unknownEndpoint)
